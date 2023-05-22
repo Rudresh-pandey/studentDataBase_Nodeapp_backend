f62278995682d97935021aa249f3b6ddc3ded6ef
Rudresh Pandey <rudreshpandey2002@gmail.com>
2023-05-22 23:04:31 +0530

    studDB added operations

--- src/studDB.js
+++ src/studDB.js
@@ -30,8 +30,73 @@ const getAllStudent = async (req, res) => {
     res.json(students);
 }
 
+const getStudent = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const student = await Student.findById(id);
+        if (!student) {
+            res.status(404).json({ message: `ERROR no student with id : ${id}` })
+        }
+        res.status(200).json({
+            message: "success Student founded",
+            student_Info: student
+        });
+    } catch (err) {
+        res.status(500).json({
+            message: "Server ERROR",
+            error: err
+        });
+    }
+}
+
+const updateStudent = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const updatedStudent = await Student.findByIdAndUpdate(id, req.body);
+        if (!updatedStudent) {
+            res.status(404).json({
+                message: `ERROR no student of id ${id}`
+            })
+        }
+        // const updateStudent = await student.save();
+        res.status(200).json({
+            message: "student info updated successfully",
+            student_Info: updatedStudent
+        });
+    } catch (err) {
+        res.status(500).json({
+            message: "Server ERROR",
+            error: err
+        })
+    }
+
+}
+
+const deleteStudent = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const deletedStudent = await Student.findByIdAndDelete(id);
+        if (!deletedStudent) {
+            res.status(404).json({
+                message: `ERROR no Student found of id: ${id}`,
+            })
+        }
+        res.status(200).json({
+            message: "student deleted successfully",
+            student_Info: deletedStudent
+        })
+    } catch (err) {
+        res.status(500).json({
+            message: "Server ERROR",
+            error: err
+        })
+    }
+}
 
 
 
 exports.addStudent = addStudent;
 exports.getAllStudent = getAllStudent;
+exports.updateStudent = updateStudent;
+exports.deleteStudent = deleteStudent;
+exports.getStudent = getStudent;
